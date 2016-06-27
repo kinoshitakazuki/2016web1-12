13dba80b200e841a245b5bfdf52c423bb00a9ea6
kinoshitakazuki <g1527025@st.t-kougei.ac.jp>
2016-06-27 17:52:03 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,10 @@
+
+
+for(var i=1; i<100; i;+){
+var li-do0cment.createElement('li');
+li.textContent=constellatio[i];
+document.getElementById('array').appendChild(li);
+}
+document.getElementById('from').onsubmit=function(){
+window.alert(document.getElementById('from').word.value);
+};
